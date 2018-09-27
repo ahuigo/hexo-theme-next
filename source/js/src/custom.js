3f64353ce834560bfec06ff09783cbecffe2cd19
ahuigo <a132811@gmail.com>
2018-09-27 17:26:47 +0800

    pic_fix

--- source/js/src/custom.js
+++ source/js/src/custom.js
@@ -1,8 +1,7 @@
 window.onload = function(){
-    document.querySelectorAll('#content posts').forEach((v)=>{
-        if(v.getAttribute('src').startsWith('/img/')){
-            v.src = v.getAttribute('src').replace(/\/img/, '/a/img')
-        }
-    });
-
+  document.querySelector('#posts').querySelectorAll('img').forEach((v)=>{
+    if(v.getAttribute('src').startsWith('/img/')){
+        v.src = v.getAttribute('src').replace(/\/img/, '/a/img')
+    }
+});
 }
